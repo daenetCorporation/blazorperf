15359ba25af193fdc71a8c2cfeb738ba95ee84e2
BuiNhatQuang <nqbui@daenet.com>
2021-05-05 13:14:37 +0200

    add Blazor Worker

---
+++ LoopPerformance/LoopPerformance.BlazorWasm/Services/LongRunningService.cs
@@ -0,0 +1,18 @@
+﻿using LoopPerformanceLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoopPerformance.BlazorWasm.Services
+{
+    public class LongRunningService
+    {
+        public bool LongRunningTask(int itemCount)
+        {
+            Helpers.SampleRun(itemCount);
+
+            return true;
+        }
+    }
+}
